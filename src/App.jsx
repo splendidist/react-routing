9046012c88a9f8e81884dd9cdf8ed8eacaef38d2
Lucy Holmes <lucyholmes08@hotmail.com>
2024-04-29 11:30:15 +0100

    catch error

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ export default function App() {
       <div className="App">
         <h1>Welcome to my website</h1>
         {/* {" "} makes a little space before the word test */}
+        {/* /test can be anything, as these pages don't exist it will bring the catch error */}
         <Link to="/">Home</Link> | <Link to="/about">About</Link> |{" "}
         <Link to="/test">Test</Link>
         <Routes>
